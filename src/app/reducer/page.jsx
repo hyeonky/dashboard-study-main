e50064d252632c079a2c974d3c01f59d7ca441f1
hyeonky <hyeon2762@naver.com>
2024-11-22 17:35:54 +0900

    counterReducer 데이터 분리

--- src/app/reducer/page.jsx
+++ src/app/reducer/page.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+'use client'
+import { ACTION_TYPE, setCount } from '@/reducers/counterReducer'
+import { useReducer, useState } from 'react'
 
-const buttonStyle = 'px-4 py-2 bg-blue-500 text-white rounded'
+const buttonStyle = 'px-4 py-2 bg-gray-300 text-white rounded'
 
 const ReducerPage = () => {
+  // const [count, setCount] = useState(0)
+
+  // const [현재상태, 파견함수] = useReducer(상태변경함수, 초기값)
+  const [count, dispatch] = useReducer(setCount, { counter: 0, name: '수량' })
+
   return (
     <div className="flex justify-center gap-4 py-5 text-4xl">
-      <button>-</button>
-      <strong className="text-blue-600">0</strong>
-      <button>+</button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION_TYPE.DECREMENT })
+        }}
+      >
+        -
+      </button>
+      <strong>
+        {count.name} : {count.counter}
+      </strong>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION_TYPE.INCREMENT })
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION_TYPE.RESET })
+        }}
+      >
+        reset
+      </button>
     </div>
   )
 }
